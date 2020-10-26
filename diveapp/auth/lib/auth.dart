2a06dd0c284c2daa069f6cc8062d0661e03e86cd
Anders Olesen <Anders2605-94@live.dk>
2020-10-26 12:36:38 +0100

    Added signin and signup page. signin button redirects to homepage

--- diveapp/auth/lib/auth.dart
+++ diveapp/auth/lib/auth.dart
@@ -1,7 +1,9 @@
 library auth;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export './src/domain/auth_service_contract.dart';
+export './src/domain/sign_up_service_contract.dart';
+export './src/domain/token.dart';
+export './src/infra/adapters/sign_up_service.dart';
+export './src/infra/api/auth_api.dart';
+export './src/infra/api/auth_api_contract.dart';
+export './src/infra/managers/auth_manager.dart';
--- diveapp/lib/main.dart
+++ diveapp/lib/main.dart
@@ -1,10 +1,12 @@
-import 'package:diveapp/pages/HomePage.dart';
+import 'package:diveapp/composition_root.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 PageController pageController = PageController(initialPage: 0);
 int currentIndex = 0;
 
 void main() {
+  CompositionRoot.configure();
   runApp(DiveApp());
 }
 
@@ -13,12 +15,14 @@ class DiveApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Dive App',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        textTheme: GoogleFonts.montserratTextTheme(Theme.of(context).textTheme),
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       // extendBody: true,
-      home: HomePage(),
+      home: CompositionRoot.composeAuthUi(),
     );
   }
 }
